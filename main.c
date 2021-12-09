16a6d1519c6d9dce5440e8c12b02b60fc114fc4a
Jackson93150 <jackjack93150@gmail.com>
2021-12-09 18:09:00 +0100

    dijkstra casser

--- main.c
+++ main.c
@@ -8,23 +8,26 @@
 #define MAX 1000
 #define MAXSUC 20
 
-struct noeud {
+struct noeud
+{
   int num;
   int nbs;
   int poids[MAXSUC];
-  struct noeud * succ[MAXSUC];
+  struct noeud *succ[MAXSUC];
 };
 
 typedef struct noeud noeud;
-typedef struct noeud * ptnoeud;
+typedef struct noeud *ptnoeud;
 
-struct lesnoeuds {
+struct lesnoeuds
+{
   int vu;
   ptnoeud ne;
 };
-typedef struct lesnoeuds legraphe [MAX];
+typedef struct lesnoeuds legraphe[MAX];
 
-struct csv{
+struct csv
+{
   char st1[MAX];
   char st2[MAX];
   char line[MAX];
@@ -34,36 +37,47 @@ typedef struct csv dict;
 dict values[MAX];
 
 int position[303][MAXSUC];
+legraphe line[303];
+int poids[303];
 
-void stockcsv(){
+void stockcsv()
+{
   FILE *fp = fopen("london.connections.csv", "r");
-  if(!fp){
+  if (!fp)
+  {
     printf("Error");
   }
   char buff[407];
   int row = 0;
   int field_count = 0;
 
-  int i =0;
-  while(fgets(buff,407,fp)){
+  int i = 0;
+  while (fgets(buff, 407, fp))
+  {
     field_count = 0;
     row++;
-    if(row == 1){
+    if (row == 1)
+    {
       continue;
     }
     char *field = strtok(buff, ",");
-    while(field){
-      if(field_count == 0){
-        strcpy(values[i].st1,field);
+    while (field)
+    {
+      if (field_count == 0)
+      {
+        strcpy(values[i].st1, field);
       }
-      if(field_count == 1){
-        strcpy(values[i].st2,field);
+      if (field_count == 1)
+      {
+        strcpy(values[i].st2, field);
       }
-      if(field_count == 2){
-        strcpy(values[i].line,field);
+      if (field_count == 2)
+      {
+        strcpy(values[i].line, field);
       }
-      if(field_count == 3){
-        strcpy(values[i].time,field);
+      if (field_count == 3)
+      {
+        strcpy(values[i].time, field);
       }
       field = strtok(NULL, ",");
       field_count++;
@@ -73,69 +87,133 @@ void stockcsv(){
   fclose(fp);
 }
 
-ptnoeud creenoeud (int x) {
+ptnoeud creenoeud(int x)
+{
   ptnoeud t;
   int suc = 0;
-  t = (ptnoeud) malloc (sizeof(noeud));
+  t = (ptnoeud)malloc(sizeof(noeud));
   assert(t);
   t->num = x;
-  for(int i = 0 ; i < 407;i++){
-    if(x == atol(values[i].st1)){
+  for (int i = 0; i < 407; i++)
+  {
+    if (x == atol(values[i].st1))
+    {
       int tmp = atol(values[i].st2);
-      position[x-1][suc] = tmp-1;
+      position[x - 1][suc] = tmp - 1;
       suc++;
     }
   }
-  if(suc == 0){
-    for(int i = 0 ; i < 407;i++){
-      if(x == atol(values[i].st2)){
+  if (suc == 0)
+  {
+    for (int i = 0; i < 407; i++)
+    {
+      if (x == atol(values[i].st2))
+      {
         int tmp = atol(values[i].st1);
-        position[x-1][suc] = tmp-1;
+        position[x - 1][suc] = tmp - 1;
         suc++;
       }
     }
   }
   t->nbs = suc;
-  if(suc != 0){
-    for(int i = 0; i <= suc ; i++){
+  if (suc != 0)
+  {
+    for (int i = 0; i <= suc; i++)
+    {
       t->succ[i] = (ptnoeud)0;
-      int p = atol(values[position[x-1][i]].time);
+      int p = atol(values[position[x - 1][i]].time);
       t->poids[i] = p;
     }
   }
-  if(suc == 0){
+  if (suc == 0)
+  {
     t->succ[0] = (ptnoeud)0;
     t->poids[0] = 0;
   }
   return t;
 }
 
-ptnoeud creeptnoeud (int x) {
+ptnoeud creeptnoeud(int x)
+{
   ptnoeud t[303];
   int i;
-  for (i = 1; i < 303; i++) {
-	  t[i-1] = creenoeud(i);
+  for (i = 1; i < 303; i++)
+  {
+    t[i - 1] = creenoeud(i);
   }
-  for(i = 1; i < 303; i++) {
-    if(t[i-1]->nbs != 0){
-      for(int j = 0 ; j <= t[i-1]->nbs;j++){
-        int tmp = position[i-1][j];
-        t[i-1]->succ[j] = t[tmp];
+  for (i = 1; i < 303; i++)
+  {
+    if (t[i - 1]->nbs != 0)
+    {
+      for (int j = 0; j <= t[i - 1]->nbs; j++)
+      {
+        int tmp = position[i - 1][j];
+        t[i - 1]->succ[j] = t[tmp];
       }
     }
   }
   return t[x];
 }
 
-int main(){
+int minPoids(){
+  int i;
+  int temp = 1000;
+  int min = -1;
+  for (i = 0; i < 303; i++){
+    if (poids[i] <= temp){
+      if (line[i]->vu == 0){
+        if (poids[i] != -1){
+          min = i;
+          temp = poids[i];
+        }
+      }
+    }
+  }
+  return min;
+}
+
+void Dijkstra(int Depart, int Destination)
+{
+  int distance = 0;
+  int antecedent[303];
+  int minline = -1;
+  for (int i = 0; i < 303; i++){
+    poids[i] = -1;
+    if (i == Depart-1){
+      poids[i] = 0;
+    }
+  }
+  while(minline != Destination-1){
+    minline = minPoids();
+    printf("%d\n",line[minline]->ne->num);
+    for(int i = 0 ; i < line[minline]->ne->nbs ;i++){
+      int nb = line[minline]->ne->succ[i]->num;
+      int pds = poids[minline]+*line[minline]->ne->succ[i]->poids;
+      if(line[nb-1]->vu == 0){
+        if(poids[nb-1] > pds || poids[nb-1] <= 0 ){
+          poids[nb-1] = poids[minline]+*line[minline]->ne->succ[i]->poids;
+          antecedent[nb-1] = minline+1;
+          line[minline]->vu = 1;
+        }
+      }
+    }
+    distance++;
+    if(distance == 100){
+      minline = Destination-1;
+    }
+  }
+}
+
+int main()
+{
   stockcsv();
   ptnoeud x;
   x = creeptnoeud(10);
-  legraphe z[303];
-  for(int i = 1 ; i < 303 ; i++){
-    z[i-1]->vu = 0;
-    z[i-1]->ne = creeptnoeud(i-1);
+  for (int i = 1; i < 303; i++)
+  {
+    line[i - 1]->vu = 0;
+    line[i - 1]->ne = creeptnoeud(i - 1);
   }
-  printf("%d\n",z[301]->ne->succ[0]->num);
+  Dijkstra(82,72);
   return 0;
 }
