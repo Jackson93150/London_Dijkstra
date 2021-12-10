4e3dc46301c94ed95daeeb168c1ced6041e5f54e
Jackson93150 <jackjack93150@gmail.com>
2021-12-10 13:05:26 +0100

    c'est mieux mais c toujours casser

--- main.c
+++ main.c
@@ -137,16 +137,12 @@ ptnoeud creeptnoeud(int x)
 {
   ptnoeud t[303];
   int i;
-  for (i = 1; i < 303; i++)
-  {
+  for (i = 1; i < 303; i++){
     t[i - 1] = creenoeud(i);
   }
-  for (i = 1; i < 303; i++)
-  {
-    if (t[i - 1]->nbs != 0)
-    {
-      for (int j = 0; j <= t[i - 1]->nbs; j++)
-      {
+  for (i = 1; i < 303; i++){
+    if (t[i - 1]->nbs != 0){
+      for (int j = 0; j <= t[i - 1]->nbs; j++){
         int tmp = position[i - 1][j];
         t[i - 1]->succ[j] = t[tmp];
       }
@@ -156,24 +152,33 @@ ptnoeud creeptnoeud(int x)
 }
 
 int minPoids(){
-  int i;
-  int temp = 1000;
-  int min = -1;
-  for (i = 0; i < 303; i++){
+  int temp = MAX;
+  int mini = -1;
+  for (int i = 0; i < 303; i++){
     if (poids[i] <= temp){
-      if (line[i]->vu == 0){
+      if (line[i]->vu !=1){
         if (poids[i] != -1){
-          min = i;
+          mini = i;
           temp = poids[i];
         }
       }
     }
   }
-  return min;
+  if(mini == 250){
+      for(int i = 0 ; i < 303;i++){
+          if(line[i]->vu !=1){
+             // printf("%d\n",poids[i]);
+          }
+      }
+  }
+  return mini;
 }
 
-void Dijkstra(int Depart, int Destination)
-{
+void Dijkstra(int Depart, int Destination){
+  for (int i = 1; i < 303; i++){
+    line[i - 1]->vu = 0;
+    line[i - 1]->ne = creeptnoeud(i - 1);
+  }
   int distance = 0;
   int antecedent[303];
   int minline = -1;
@@ -185,20 +190,20 @@ void Dijkstra(int Depart, int Destination)
   }
   while(minline != Destination-1){
     minline = minPoids();
-    printf("%d\n",line[minline]->ne->num);
+   // printf("%d\n",line[minline]->ne->num);
     for(int i = 0 ; i < line[minline]->ne->nbs ;i++){
       int nb = line[minline]->ne->succ[i]->num;
       int pds = poids[minline]+*line[minline]->ne->succ[i]->poids;
       if(line[nb-1]->vu == 0){
         if(poids[nb-1] > pds || poids[nb-1] <= 0 ){
-          poids[nb-1] = poids[minline]+*line[minline]->ne->succ[i]->poids;
-          antecedent[nb-1] = minline+1;
-          line[minline]->vu = 1;
+          poids[nb-1] = pds;
+          antecedent[nb-1] = line[minline]->ne->num;
         }
       }
     }
+    line[minline]->vu = 1;
     distance++;
-    if(distance == 100){
+    if(distance == 303){
       minline = Destination-1;
     }
   }
@@ -207,13 +212,6 @@ void Dijkstra(int Depart, int Destination)
 int main()
 {
   stockcsv();
-  ptnoeud x;
-  x = creeptnoeud(10);
-  for (int i = 1; i < 303; i++)
-  {
-    line[i - 1]->vu = 0;
-    line[i - 1]->ne = creeptnoeud(i - 1);
-  }
-  Dijkstra(82,72);
+  Dijkstra(255,196);
   return 0;
 }
